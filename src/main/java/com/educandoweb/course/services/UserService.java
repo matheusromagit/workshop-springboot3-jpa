3e03dd18e198a5a62bd4fff141fe1bb5461105c5
matheus rodrigues lima <matheusroma.ctt@gmail.com>
2024-11-22 10:32:20 -0300

    Service layer, component registration with loggin

---
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -0,0 +1,31 @@
+package com.educandoweb.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educandoweb.course.entities.User;
+import com.educandoweb.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        List<User> users = repository.findAll();
+        logger.info("Users retrieved: {}", users);
+        return users;
+    }
+    public User findById(Long id) {
+    	Optional<User> obj = repository.findById(id);
+    	return obj.get();
+    }
+}
