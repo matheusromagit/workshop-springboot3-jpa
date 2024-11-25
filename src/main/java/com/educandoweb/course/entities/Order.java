ad52ad74ab4a042b962a39ca648f73d53a5b1d51
matheus rodrigues lima <matheusroma.ctt@gmail.com>
2024-11-25 10:31:30 -0300

    Order, Instant, ISO 8601 PART 3

--- src/main/java/com/educandoweb/course/entities/Order.java
+++ src/main/java/com/educandoweb/course/entities/Order.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 import java.time.Instant;
 import java.util.Objects;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -23,6 +26,8 @@ public class Order implements Serializable {
     private Long id;
     private Instant moment;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
+    
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
